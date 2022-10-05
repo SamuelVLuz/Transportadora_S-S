83ad2572ab48838e47aaece1aa270bea7a9e56cc
SamuelVLuz <63885675+SamuelVLuz@users.noreply.github.com>
2022-10-05 10:50:28 -0300

    Add files via upload

---
+++ Transportadora/inicial.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>S&S transporte</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/Style.css'>
+    <script src="java.js" defer></script>
+</head>
+
+<!--Cabeçalho do site-->
+<?php
+	require_once("cabecalho.php");
+?>
+ 
+<body>
+    <div tabindex="0" class="content" onfocus="closeSidebar()" id="content">
+        <h1>Agilidade e comprometimento</h1>
+        <h1>no transporte municipal e estadual</h1>
+        <button class="botao" type="submit">Conheça mais sobre nós</button>
+    </div>
+    <footer class="footer">
+        <div class="boxs">
+            <h2>principal</h2>
+            <ul>
+                <li><a href="#"></a>Contato</li>
+                <li><a href="#"></a>Email</li>
+                <li><a href="#"></a>Telefone</li>
+            </ul>
+        </div>
+        <div class="boxs">
+            <h2>secundario</h2>
+            <ul>
+                <li><a href="#"></a>blog</li>
+                <li><a href="#"></a>FAQ</li>
+                <li><a href="#"></a>chat</li>
+            </ul>
+        </div>
+        <div class="boxs">
+            <h2>terceario</h2>
+            <ul>
+                <li><a href="#"></a>home</li>
+                <li><a href="#"></a>produto</li>
+                <li><a href="#"></a>download</li>
+            </ul>
+        </div>
+        <div class="boxs">
+            <h2>Textos</h2>
+            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque dicta minima doloribus fugiat minus maiores, nihil ipsam cumque iste enim, harum esse. Perferendis veniam minus earum iusto molestias ipsa nisi?</p>
+        </div>
+        <div>
+            <h2>Logo</h2>
+            <div class="logo"></div>
+        </div>
+    </footer>
+</body>
+</html>
