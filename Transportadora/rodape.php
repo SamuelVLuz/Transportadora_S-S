1c4078e457121b55cd13df92c753490f06f13a40
SamuelVLuz <63885675+SamuelVLuz@users.noreply.github.com>
2022-10-05 11:01:00 -0300

    Add files via upload

---
+++ Transportadora/rodape.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+        <div class="boxs">
+            <h2>principal</h2>
+            <ul>
+                <li><a href="#"></a>Contato</li>
+                <li><a href="#"></a>Email</li>
+                <li><a href="#"></a>Telefone</li>
+            </ul>
+        </div>
+        <div class="boxs">
+            <h2>secundario</h2>
+            <ul>
+                <li><a href="#"></a>blog</li>
+                <li><a href="#"></a>FAQ</li>
+                <li><a href="#"></a>chat</li>
+            </ul>
+        </div>
+        <div class="boxs">
+            <h2>terceario</h2>
+            <ul>
+                <li><a href="#"></a>home</li>
+                <li><a href="#"></a>produto</li>
+                <li><a href="#"></a>download</li>
+            </ul>
+        </div>
+        <div class="boxs">
+            <h2>Textos</h2>
+            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque dicta minima doloribus fugiat minus maiores, nihil ipsam cumque iste enim, harum esse. Perferendis veniam minus earum iusto molestias ipsa nisi?</p>
+        </div>
+        <div>
+            <h2>Logo</h2>
+            <div class="logo"></div>
+    </div>
+</footer>
